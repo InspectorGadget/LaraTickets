e283bbe1d690ae6b2ddae4c9ee6e6760fff2999c
InspectorGadget™ <igadget28@gmail.com>
2019-10-26 11:01:02 +0800

    Fixed Category Migration & Model

--- app/Category.php
+++ app/Category.php
@@ -7,7 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
 
-    protected $fillable = ['name'];
+    protected $fillable = [
+        'name'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
 
     public function tickets() {
         $this->hasMany(Ticket::class);
--- database/migrations/2019_10_23_133155_create_categories_table.php
+++ database/migrations/2019_10_23_133155_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('category');
+            $table->string('name');
             $table->timestamps();
         });
     }
